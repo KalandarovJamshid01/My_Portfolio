9d0391dc813fbdf0b4e4cef2c4bbeda601996edc
Jamshid <kalandarovjamshid01@gmail.com>
2024-10-29 20:13:05 +0500

    okay "

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Jamshid.</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a Nodejs Backend developer
+            I am a full-stack developer
             <br className="sm:block hidden" />
             from Tashkent
           </p>
